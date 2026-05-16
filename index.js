609743e6eddaf49d371e66de78a70ff53f4b436d
Marucus_1 <132256984+Botinokiller@users.noreply.github.com>
2026-05-16 23:33:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let Fullname = 'Marcus Miller';
+let age = 25;
+let student = false;
+
+
+document.getElementById('pt1').textContent = Fullname;
+document.getElementById('pt2').textContent = age;
+document.getElementById('pt3').textContent = student;
